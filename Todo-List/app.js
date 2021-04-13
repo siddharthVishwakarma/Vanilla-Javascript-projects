2d2090af43c79c025025f7f5b3511d588476969c
Siddharth Vishwakarma <sid.sv14@gmail.com>
2021-04-14 00:03:20 +0530

    two new feature added

--- Todo-List/app.js
+++ Todo-List/app.js
@@ -3,7 +3,7 @@ const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-task');
 const filter = document.querySelector('#filter');
-const taskInput = document.querySelector('#task')
+const taskInput = document.querySelector('#task');
 
 // Load all event listners
 loadEventListners();
@@ -11,9 +11,13 @@ loadEventListners();
 function loadEventListners(){
     // Add task event
     form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
+    // Clear task event
+    clearBtn.addEventListener('click',clearTask);
 }
 
-// Add Task
+//*Function for adding Task
 function addTask(e){
     if(taskInput.value === ''){
         alert('Please add a task');
@@ -35,17 +39,41 @@ function addTask(e){
     link.className = 'delete-item secondary-content';
 
     // Add icon html
-    link.innerHTML = '<i class="fa fa-remove"></i>'
+    link.innerHTML = '<i class="fa fa-remove"></i>';
 
     // Append the link to li
     li.appendChild(link);
 
     // Append li to ul
     taskList.appendChild(li);
-    
+
     // Clear input
     taskInput.value = '';
 
 
     e.preventDefault();
+
+}
+
+// *Function for clearing Tasks
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('Do you want to delete the task')){
+            // console.log(e.target)
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+//* Function for removing task
+function clearTask(){
+    // first way
+    // taskList.innerHTML = '';
+
+    // second way Faster method
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+
+
 }
