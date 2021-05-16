15df7ef649f38465a23bd0da269c22f09383434e
Siddharth Vishwakarma <sid.sv14@gmail.com>
2021-05-16 21:37:26 +0530

    clear list added

--- Book-List/appES5.js
+++ Book-List/appES5.js
@@ -28,6 +28,13 @@ UI.prototype.addBookToList = function(book){
     list.appendChild(row);
 }
 
+// Clear Fields
+UI.prototype.clearFields = function(){
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
+
 // Event listners
 document.getElementById('book-form').addEventListener('submit', function(e){
     // Get form values
@@ -44,6 +51,8 @@ document.getElementById('book-form').addEventListener('submit', function(e){
     // Add book to list
     ui.addBookToList(book);
 
+    // Clear fields
+    ui.clearFields();
 
     e.preventDefault();
 })
