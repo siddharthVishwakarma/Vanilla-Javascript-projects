2c32661b6359a20a5833e2019350332cdcb98715
Siddharth Vishwakarma <sid.sv14@gmail.com>
2021-05-18 19:40:41 +0530

    update 2.0

--- Book-List/appES5.js
+++ Book-List/appES5.js
@@ -28,6 +28,26 @@ UI.prototype.addBookToList = function(book){
     list.appendChild(row);
 }
 
+// Show Alert
+UI.prototype.showAlert = function(message, className){
+    // Create div
+    const div = document.createElement('div');
+    // Add class
+    div.className = `alert ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get Parent
+    const container = document.querySelector('.container');
+    // Get form
+    const form = document.getElementById('book-form');
+    // Insert Alert
+    container.insertBefore(div, form);
+    // Message timeout after 3 sec
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+    }, 2000);
+}
+
 // Clear Fields
 UI.prototype.clearFields = function(){
     document.getElementById('title').value = '';
@@ -48,11 +68,20 @@ document.getElementById('book-form').addEventListener('submit', function(e){
     // Instantiate UI
     const ui = new UI();
 
-    // Add book to list
-    ui.addBookToList(book);
+    // Validate
+    if(title === '' || author === '' || isbn === ''){
+        // Error alert
+        ui.showAlert('Please fill in the all fields', 'error');  
+    } else {
+        // Add book to list
+        ui.addBookToList(book);
 
-    // Clear fields
-    ui.clearFields();
+        // Show success
+        ui.showAlert('Book Added!', 'success'); 
+
+        // Clear fields
+        ui.clearFields();
+    }
 
     e.preventDefault();
 })
