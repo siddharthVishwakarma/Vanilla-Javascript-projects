a9532e91d7cc1e47a7cfa1601143b7ed00bbb6d5
Siddharth Vishwakarma <sid.sv14@gmail.com>
2021-05-20 23:59:00 +0530

    Event listners added

--- Book-List/appES6.js
+++ Book-List/appES6.js
@@ -57,3 +57,48 @@ class UI {
     }
 
 }
+
+// Event listners for add books.
+document.getElementById('book-form').addEventListener('submit', function(e){
+    // Get form values
+    const title = document.getElementById('title').value,
+          author = document.getElementById('author').value,
+          isbn = document.getElementById('isbn').value
+
+    // Instantiate book
+    const book = new Book(title, author, isbn);
+
+    // Instantiate UI
+    const ui = new UI();
+
+    // Validate
+    if(title === '' || author === '' || isbn === ''){
+        // Error alert
+        ui.showAlert('Please fill in the all fields', 'error');  
+    } else {
+        // Add book to list
+        ui.addBookToList(book);
+
+        // Show success
+        ui.showAlert('Book Added!', 'success'); 
+
+        // Clear fields
+        ui.clearFields();
+    }
+
+    e.preventDefault();
+})
+
+// Event listner for delete books
+document.getElementById('book-list').addEventListener('click', function(e){
+    // Instantiate UI
+    const ui = new UI();
+
+    // Delete book
+    ui.deleteBook(e.target);
+
+    // Show message
+    ui.showAlert('Book Removed!', "success");
+
+    e.preventDefault();
+})
